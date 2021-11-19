780c071305f3f3801ad9fa345a25b872e8f61539
seva <seva.netrebchenko@gmail.com>
2021-11-19 04:28:40 -0800

    Third gif for project completed.

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,9 @@ int main(int argc, char* argv[]) {
 //        return 1;
 //    }
 
-    img::image image("assets/images/the_weeknd.jpg");
+    img::image image("assets/images/journey.jpg");
 
-    for (int k = 2; k <= 16; ++k) {
+    for (int k = 2; k <= 32; ++k) {
         std::cout << "processing: " << k << std::endl;
         image.process().k_means(k, true).save();
     }
