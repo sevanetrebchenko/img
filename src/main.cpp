2e522f66cc50f1a5e338ab3a73c652ccb74dae94
seva <seva.netrebchenko@gmail.com>
2022-01-10 18:29:45 -0800

    More generated images.

--- src/main.cpp
+++ src/main.cpp
@@ -6,30 +6,30 @@
 
 int main(int argc, char* argv[]) {
 
-    img::image image("assets/images/journey.jpg");
-//
-//    image.process().to_grayscale().save();
-//
-//    image.process().to_lower_resolution(20, 20).save();
-//
-//    for (int k = 2; k <= 16; ++k) {
-//        image.process().k_means(k).save();
-//    }
-//
-//    image.process().dither_error_diffusion().save();
-//    image.process().dither_floyd_steinberg().save();
-//    image.process().dither_false_floyd_steinberg().save();
-//    image.process().dither_jarvis_judice_ninke().save();
-//    image.process().dither_stucki().save();
-//    image.process().dither_atkinson().save();
-//    image.process().dither_burkes().save();
-//    image.process().dither_sierra().save();
-//    image.process().dither_two_row_sierra().save();
-//    image.process().dither_sierra_lite().save();
-//    image.process().dither_bayer(2, 2).save();
-//    image.process().dither_bayer(4, 4).save();
-//    image.process().dither_bayer(8, 8).save();
-//    image.process().dither_bayer(4, 6).save();
+    img::image image("assets/images/desert.png");
+
+    image.process().to_grayscale().save();
+
+    image.process().to_lower_resolution(20, 20).save();
+
+    for (int k = 2; k <= 16; ++k) {
+        image.process().k_means(k).save();
+    }
+
+    image.process().dither_error_diffusion().save();
+    image.process().dither_floyd_steinberg().save();
+    image.process().dither_false_floyd_steinberg().save();
+    image.process().dither_jarvis_judice_ninke().save();
+    image.process().dither_stucki().save();
+    image.process().dither_atkinson().save();
+    image.process().dither_burkes().save();
+    image.process().dither_sierra().save();
+    image.process().dither_two_row_sierra().save();
+    image.process().dither_sierra_lite().save();
+    image.process().dither_bayer(2, 2).save();
+    image.process().dither_bayer(4, 4).save();
+    image.process().dither_bayer(8, 8).save();
+    image.process().dither_bayer(4, 6).save();
 
     image.process().voronoi(50).save();
     image.process().voronoi(100).save();
@@ -38,6 +38,7 @@ int main(int argc, char* argv[]) {
     image.process().voronoi(1000).save();
     image.process().voronoi(2000).save();
     image.process().voronoi(5000).save();
+    image.process().voronoi(10000).save();
 
     return 0;
 }
